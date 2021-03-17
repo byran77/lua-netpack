2b2b1704c6a4394e056f851cb7abe3a571f552d8
byran77 <byran77@outlook.com>
2021-03-17 21:41:09 +0800

    Modify examples

--- example/test.lua
+++ example/test.lua
@@ -1,2 +1,17 @@
 package.path = package.path..";../?.lua"
 local net_pack = require"net_pack"
+
+local pack = net_pack.new()     --Create a packet object
+--[[
+pack.wDataLen = 
+pack.byFlag = 
+pData =
+
+write(pack:pack())     
+--]]                            --Send the packet
+
+--[[
+local data = read()             --Receive the data
+--]]
+
+local pack = net_pack.unpack_data(data) --Parse the received data 
